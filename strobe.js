cf2eee17a14147121a0e5eb41f39209265e9c071
Dan Guest <dguest@cern.ch>
2016-08-18 23:06:39 +0200

    more awesome

--- strobe.js
+++ strobe.js
@@ -1,30 +1,12 @@
-var text1=document.body.style.backgroundColor;
-var text2=document.body.style.color;
-var boolean_value1=true;
-var boolean_value2=true;
+
+var colors = ["red", "blue", "black", "green", "azure", "grey", "pink"]
+var idx = 0;
 function strobe()
 {
- if (boolean_value2)
- {
-  document.body.style.backgroundColor = boolean_value1 ? 'black' : 'white';
-  document.body.style.color = boolean_value1 ? 'white' : 'black';
-  boolean_value1=!boolean_value1;
-  setTimeout(strobe, 70); //in milliseconds
- }
- else
- {
-  document.body.style.backgroundColor=text1;
-  document.body.style.color=text2;
-  // alert("The else statement was executed...boolean_value2 evaluates to false");
- }
+    document.body.style.backgroundColor = colors[Math.floor((Math.random()*7))];
+    document.getElementById('blah').style.color = colors[Math.floor(Math.random()*7)];
+    idx++;
+    setTimeout(strobe, 300); //in milliseconds
+    
 }
 
-function stop_strobe()
-{
- boolean_value2=false;
-}
-
-strobe();
-
-
-window.onclick = stop_strobe;
