1ec8aca50cd4c461a93d7006d9c996dd5b4a0bc8
Dan Guest <dguest@cern.ch>
2016-08-19 09:34:55 +0200

    add ability to set delay

--- strobe.js
+++ strobe.js
@@ -1,12 +1,16 @@
 
 var colors = ["red", "blue", "black", "green", "azure", "grey", "pink"]
 var idx = 0;
+var params = new URLSearchParams(location.search.slice(1))
+var delay = 300
+if (params.has('delay')) {
+    delay = params.get('delay')
+}
 function strobe()
 {
     document.body.style.backgroundColor = colors[Math.floor((Math.random()*7))];
     document.getElementById('blah').style.color = colors[Math.floor(Math.random()*7)];
     idx++;
-    setTimeout(strobe, 300); //in milliseconds
-    
+    setTimeout(strobe, delay); //in milliseconds
 }
 
