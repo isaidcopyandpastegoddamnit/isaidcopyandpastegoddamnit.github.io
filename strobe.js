daf7c30476032006ce729112f1f2527f0b5527f8
Dan Guest <dguest@cern.ch>
2016-08-19 09:53:48 +0200

    add seizureMode

--- strobe.js
+++ strobe.js
@@ -1,16 +1,26 @@
 
 var colors = ["red", "blue", "black", "green", "azure", "grey", "pink"]
-var idx = 0;
-var params = new URLSearchParams(location.search.slice(1))
-var delay = 300
-if (params.has('delay')) {
-    delay = params.get('delay')
+var bg_idx = 0;
+var params = new URLSearchParams(location.search.slice(1));
+var delay_back = 300;
+var delay_front = 300;
+if (params.has('fg')) {
+    delay_front = params.get('fg');
 }
-function strobe()
-{
-    document.body.style.backgroundColor = colors[Math.floor((Math.random()*7))];
-    document.getElementById('blah').style.color = colors[Math.floor(Math.random()*7)];
-    idx++;
-    setTimeout(strobe, delay); //in milliseconds
+if (params.has('bg')) {
+    delay_back = params.get('bg');
 }
-
+if (params.has('seizureMode') ) {
+    delay_back = 100;
+    delay_front = 0;
+}
+function strobe_back(){
+    var idx = Math.floor((Math.random()*colors.length));
+    document.body.style.backgroundColor = colors[idx];
+    setTimeout(strobe_back, delay_back); //in milliseconds
+}
+function strobe_front() {
+    var idx = Math.floor((Math.random()*colors.length));
+    document.getElementById('blah').style.color = colors[idx];
+    setTimeout(strobe_front, delay_front); //in milliseconds
+}
